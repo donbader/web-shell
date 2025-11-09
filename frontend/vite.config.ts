e01f8210c8a6b8aed894753eb53fb28bf8c0f156
Corey Feng <corey.feng@fazzfinancial.com>
2025-11-09 09:32:50 +0800

    Fix port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3377,
+    port: 5173,
   },
   base: process.env.VITE_BASE_PATH || '/',
 })
